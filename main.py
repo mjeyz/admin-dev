8778757acd5dec2cdd5de910fdf14b9b2d96387a
Mudassir jamshais <thisismjeyz@gmail.com>
2024-12-06 07:20:34 +0400

    Snake_game

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello")
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("My Snake game")
+screen.setup(width=600, height=600)
+
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+segments = []  # List to store each segment of the snake
+
+for position in starting_position:
+    new_turtle = Turtle("square")
+    new_turtle.penup()  # Prevents drawing lines when moving
+    new_turtle.goto(position)
+    new_turtle.color("white")
+    segments.append(new_turtle)  # Add the segment to the list
+
+screen.exitonclick()
