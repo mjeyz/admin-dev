bdbcb3fd437e335191ae1a551c7fb86735929941
Mudasir Jamshaid <thisismjeyz@gmail.com>
2024-12-07 11:04:54 +0400

    day 20 Snake game

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
 
+# Screen setup
 screen = Screen()
 screen.bgcolor("black")
-screen.title("My Snake game")
+screen.title("My Snake Game")
 screen.setup(width=600, height=600)
+screen.tracer(0)
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-segments = []  # List to store each segment of the snake
+# Initialize the game
+snake = Snake()
+food = Food()
 
-for position in starting_position:
-    new_turtle = Turtle("square")
-    new_turtle.penup()  # Prevents drawing lines when moving
-    new_turtle.goto(position)
-    new_turtle.color("white")
-    segments.append(new_turtle)  # Add the segment to the list
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+is_on_game = True
+
+while is_on_game:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
